dbec47be2ba704cbfcb7ca6b5707168ab617d94a
Larissa Sevilha <sevilha.larissa10@gmail.com>
2024-10-29 22:06:44 -0300

    bugfix/ add ne buton

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const button = document.getElementById('myButton');
     button.addEventListener('click', () => {
-        alert('Botão clicado!');
+        alert('Botão 1 clicado!');
+    });
+
+    const secondButton = document.getElementById('mySecondButton');
+    secondButton.addEventListener('click', () => {
+        alert('Botão 2 clicado!');
     });
 });
